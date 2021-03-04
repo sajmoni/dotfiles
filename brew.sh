c47765f58b23e5c044e63d1e2110b949ed310764
Simon Lagos <simon.lagos.sallhed@gmail.com>
2021-03-04 12:08:52 +0100

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,9 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install node
+brew install autojump
+brew install tig
+brew install zsh-syntax-highlighting
+brew install hammerspoon
+brew install thefuck
+brew install yarn
