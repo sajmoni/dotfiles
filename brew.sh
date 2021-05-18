9142c3b11eae36aadcfd47853b56bd1bc6d75c6c
Simon Lagos <simon.lagos.sallhed@gmail.com>
2021-05-18 21:05:40 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,9 +1,23 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install node
+
+# Easier to update this version than the built-in apple one
+brew install git
+
+# Jump between folders with j
 brew install autojump
+
+# Git client for the terminal
 brew install tig
+
 brew install zsh-syntax-highlighting
+
+# Automate macOS
 brew install hammerspoon
+
+# Execute the suggestion of a previous command
 brew install thefuck
+
+# Maybe time to upgrade to npm?
 brew install yarn
