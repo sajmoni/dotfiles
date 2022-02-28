99457d65b4ac5a35590e38ae4c0612ec923be4cf
Simon Lagos <simon.lagos.sallhed@gmail.com>
2022-02-28 13:46:55 +0100

    Add createSymlinks script

---
+++ script/createSymlinks.sh
@@ -0,0 +1,6 @@
+ln -s ~/.dotfiles/script/.zshrc ~/.zshrc
+mkdir ~/.hammerspoon
+ln -s ~/.dotfiles/script/.hammerspoon/init.lua ~/.hammerspoon/init.lua
+ln -s ~/.dotfiles/script/.hyper.js ~/.hyper.js
+ln -s ~/.dotfiles/script/.tigrc ~/.tigrc
+ln -s ~/.dotfiles/script/.vimrc ~/.vimrc
--- script/run.sh
+++ script/run.sh
@@ -1,12 +1,7 @@
 set -e
 
 echo "== Symlink files =="
-ln -s ~/.dotfiles/script/.zshrc ~/.zshrc
-mkdir ~/.hammerspoon
-ln -s ~/.dotfiles/script/.hammerspoon/init.lua ~/.hammerspoon/init.lua
-ln -s ~/.dotfiles/script/.hyper.js ~/.hyper.js
-ln -s ~/.dotfiles/script/.tigrc ~/.tigrc
-ln -s ~/.dotfiles/script/.vimrc ~/.vimrc
+./script/createSymlinks.sh
 echo ""
 
 echo "== Install Homebrew =="
