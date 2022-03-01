b78f76b27dcbe8820c9565fc5dfb029a5ca2a035
Simon Lagos <simon.lagos.sallhed@gmail.com>
2022-03-01 17:05:41 +0100

    Add enhanced tab plugin to Hyper

--- config/.hyper.js
+++ config/.hyper.js
@@ -188,6 +188,9 @@ module.exports = {
     webGLRenderer: true,
 
     // for advanced config flags please refer to https://hyper.is/#cfg
+    hypertabs: {
+      tabIconsColored: true,
+    },
   },
 
   // a list of plugins to fetch and install from npm
@@ -196,7 +199,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-pane", "hypercwd"],
+  plugins: ["hyper-pane", "hypercwd", "hyper-tabs-enhanced"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
