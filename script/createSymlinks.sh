faab04eb352da787e135e6368ea687e5dbf9fba8
Simon Lagos <simon.lagos.sallhed@gmail.com>
2022-07-26 18:12:01 +0200

    Simplify symlink creation

--- script/createSymlinks.sh
+++ script/createSymlinks.sh
@@ -1,6 +1,10 @@
-ln -sfn ~/.dotfiles/config/.zshrc ~/.zshrc
-ln -sfn ~/.dotfiles/config/.hammerspoon/init.lua ~/.hammerspoon/init.lua
-ln -sfn ~/.dotfiles/config/.hyper.js ~/.hyper.js
-ln -sfn ~/.dotfiles/config/.tigrc ~/.tigrc
-ln -sfn ~/.dotfiles/config/.vimrc ~/.vimrc
-ln -sfn ~/.dotfiles/config/.gitconfig ~/.gitconfig
+createSymlink() {
+  ln -sfn ~/.dotfiles/config/$1 ~/$1
+}
+
+createSymlink ".zshrc"
+createSymlink ".hammerspoon/init.lua"
+createSymlink ".hyper.js"
+createSymlink ".tigrc"
+createSymlink ".vimrc"
+createSymlink ".gitconfig"
