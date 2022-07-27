e9ba0911244a4cb7f1dcbe84db2395d950626a22
Simon Lagos <simon.lagos.sallhed@gmail.com>
2022-07-27 17:12:21 +0200

    Simplify npm script

--- script/npm.sh
+++ script/npm.sh
@@ -1,4 +1,8 @@
-npm install --global npm-run-path-cli
-npm install --global git-ensure
-npm install --global taze
-npm install --global switch-branch-cli
+install() {
+  npm install --location=global $1
+}
+
+install "npm-run-path-cli"
+install "git-ensure"
+install "taze"
+install "switch-branch-cli"
