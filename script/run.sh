a0a3b282e636b4cedbfa2475b6b2723df02b011b
Simon Lagos <simon.lagos.sallhed@gmail.com>
2022-08-02 08:51:55 +0200

    Add gh alias to view PR on GitHub

--- script/run.sh
+++ script/run.sh
@@ -20,6 +20,7 @@ echo ""
 
 echo "== Misc =="
 gh alias set home "repo view --web"
+gh alias set prv "pr view --web"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo ""
